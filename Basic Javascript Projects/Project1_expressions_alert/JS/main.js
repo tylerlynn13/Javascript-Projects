18d4a397eec83bc64d576ab4dd9eed64f5cf4f9f
tylerlynn13 <99543167+tylerlynn13@users.noreply.github.com>
2022-03-17 09:39:58 -0400

    index

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,5 +1,14 @@
-var A = "mylan"
-var s = "Josh"
-var d = "tyler"
+var A = "mylan"  //assigning a variable
+var s = "Josh" //assigning a variable
+var d = "tyler" //assigning a variable
 
-document.write(A,s,d);
+document.write(7 + 37)  //expression
+
+document.write(A, s, d) //using the variables
+
+window.alert("HI")
+
+document.write("Pine" + "apple")
+
+var hi = "<span style='color:#ff0000'>hi</span>";
+document.write(hi);
