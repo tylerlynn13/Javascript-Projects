c6023e124ccd5ed9958d604ca72f049c0c1029df
tylerlynn13 <99543167+tylerlynn13@users.noreply.github.com>
2022-03-18 14:04:01 -0400

    page69

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+function My_First_Function() {
+    var str = "Button";
+    document.getElementbyid("Button_Text").innerHTML = str;
+}
+
+function myFunction() {
+    var sentence = "I am learning" ;
+    sentence += " a lot from this book!" ;
+    document.getElementById("Concatenate") .innerHTML = sentence ;
+}
+
+
