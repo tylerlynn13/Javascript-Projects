adef9806dfaa4a8753977cd22d2a6a8e917b91ea
tylerlynn13 <99543167+tylerlynn13@users.noreply.github.com>
2022-03-26 14:03:39 -0400

    95

--- Basic Javascript Projects/Project3_math_operators/main.js
+++ Basic Javascript Projects/Project3_math_operators/main.js
@@ -1,36 +1,36 @@
 function addition_Function() {
   var addition = 2 + 2;
-  document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+  document.getElementById("Math1").innerHTML = "2 + 2 = " + addition;
 }
 
 function subtraction_Function() {
   var Subtraction = 5 - 2;
-  document.getElementById("Math").innerHTML = "5 - 2 = " + Subtraction;
+  document.getElementById("Math2").innerHTML = "5 - 2 = " + Subtraction;
 }
 
 function multiplication() {
   var simple_Math = 6 * 8;
-  document.getElementById("Math").innerHTML = "6 x 8 = " + simple_Math
+  document.getElementById("Math3").innerHTML = "6 x 8 = " + simple_Math
 }
 
 function division() {
   var simple_Math = 48 / 6;
-  document.getElementById("Math").innerHTML = "48 / 6 = " + simple_Math;
+  document.getElementById("Math4").innerHTML = "48 / 6 = " + simple_Math;
 }
 
 function more_Math() {
   var simple_Math = (1+2) * 10 / 2-5;
-  document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math;
+  document.getElementById("Math5").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math;
 }
 
 function modulus_Operator() {
   var simple_Math = 25 % 6;
-  document.getElementById("Math").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
+  document.getElementById("Math6").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math;
 }
 
 function negation_Operator() {
   var x = 10;
-  document.getElementById("Math").innerHTML= -x;
+  document.getElementById("Math7").innerHTML= -x;
 }
 
 var x = 5;
