014c492aaaf11a8d87eb02b415b1a57d797d60a0
tylerlynn13 <99543167+tylerlynn13@users.noreply.github.com>
2022-04-01 15:24:29 -0400

    countdown and slideshow

---
+++ Basic Javascript Projects/Project9_countdown_slideshow/countdown.js
@@ -0,0 +1,14 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Times Up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+}
