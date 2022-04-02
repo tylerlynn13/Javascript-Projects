ba2e377033aad73cfc4963670576573ef41b2cfb
tylerlynn13 <99543167+tylerlynn13@users.noreply.github.com>
2022-04-02 13:43:17 -0400

    172

---
+++ Basic Javascript Projects/Project9_countdown_slideshow/defer1.js
@@ -0,0 +1,6 @@
+function slice_Method() {                   //slice method
+    var Sentence = "All work and no play makes Johny a dull boy.";
+    var Section = Sentence.slice(27,33);
+    document.getElementById("Slice").innerHTML = Section;
+}
+
